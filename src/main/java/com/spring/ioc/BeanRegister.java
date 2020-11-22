be54c46d2fbb1c86097821154ff574c168f64239
Ahusky250 <67196827+Ahusky250@users.noreply.github.com>
2020-11-22 13:42:15 +0800

    Create BeanRegister.java

---
+++ src/main/java/com/spring/ioc/BeanRegister.java
@@ -0,0 +1,20 @@
+package com.spring.ioc;
+
+import com.spring.ioc.support.BeanDefinition;
+
+import java.util.List;
+
+public interface BeanRegister {
+    /**
+     * 向工厂内注册BeanDefinition
+     * @param bds
+     */
+    void registBeanDefinition(List<BeanDefinition> bds);
+
+    /**
+     * 向工厂内注册bean实例对象
+     * @param id
+     * @param instance
+     */
+    void registInstanceMapping(String id,Object instance);
+}
