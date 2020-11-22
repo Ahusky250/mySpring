80283658355dc18f696a9f383a304738d1699c74
Ahusky250 <67196827+Ahusky250@users.noreply.github.com>
2020-11-22 13:36:38 +0800

    Create Model.java

---
+++ src/main/java/com/shiyan2/controller/Model.java
@@ -0,0 +1,13 @@
+package com.shiyan2.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Model {
+    private String filename;
+    private String filepath;
+}
