b8ccc16749f1df8e4cb7999d720e23ad08d5bdd1
Ahusky250 <67196827+Ahusky250@users.noreply.github.com>
2020-11-22 13:40:19 +0800

    Add files via upload

---
+++ src/main/java/com/spring/annotation/Component.java
@@ -0,0 +1,10 @@
+package com.spring.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+public @interface Component {
+    String value() default "";
+}
